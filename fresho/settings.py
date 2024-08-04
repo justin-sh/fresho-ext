de2082a7a834b9f93bb2d9cd48b516c217412015
Justin LI <lijt2007@hotmail.com>
2024-08-04 22:01:55 +0930

    add customer search box and filter

--- fresho/settings.py
+++ fresho/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'fresho.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
--- orders/admin.py
+++ orders/admin.py
@@ -2,12 +2,42 @@ from django.contrib import admin
 from .models import Order
 
 
+class CustomerNameFilter(admin.SimpleListFilter):
+    title = 'Customer name'
+    parameter_name = 'customer_name'
+    # template = 'admin_input_filter.html'
+
+    def lookups(self, request, model_admin):
+        return ((None, None),)
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value:
+            return queryset.filter(receiving_company_name__icontains=value)
+
+
+class ProductsFilter(admin.SimpleListFilter):
+    title = "Product"
+    parameter_name = "prd_name"
+    # template = "admin_input_filter.html"
+
+    def lookups(self, request, model_admin):
+        return ((None, None),)
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value:
+            return queryset.filter(products__icontains=value)
+
+
 class OrdersAdmin(admin.ModelAdmin):
     list_display = ('order_number', 'receiving_company_name', 'delivery_date')
-    # list_filter = ('delivery_date',)
+    list_filter = [CustomerNameFilter, ProductsFilter]
     search_fields = ('receiving_company_name',)
     list_per_page = 200
     ordering = ("-delivery_date",)
+    actions_on_top = False
+    actions = None
 
 
 admin.site.register(Order, OrdersAdmin)
