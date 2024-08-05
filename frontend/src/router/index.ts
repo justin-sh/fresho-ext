2f79a13563a80822aa46bc0cf9d5fa0167c1dfe8
Justin LI <lijt2007@hotmail.com>
2024-08-05 21:18:28 +0930

    add static and logs direcotry and update vite config

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// @ts-ignore
 import HomeView from '../views/HomeView.vue'
+// @ts-ignore
 import OrdersUploadView from '../views/OrdersUploadView.vue'
 
+// @ts-ignore
+// @ts-ignore
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: '/static',
+    plugins: [
+        vue(),
+        vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    build: {
+        outDir: '../static/'
     }
-  }
 })
